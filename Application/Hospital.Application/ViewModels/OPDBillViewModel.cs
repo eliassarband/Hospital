88731b945ff5ba041439b1e54a4aa2ad82f983f0
Elias Sarband <elias.sarband@gmail.com>
2023-02-21 17:38:51 +0330

    update opd bill define form with telerik components

--- Application/Hospital.Application/ViewModels/OPDBillViewModel.cs
+++ Application/Hospital.Application/ViewModels/OPDBillViewModel.cs
@@ -12,13 +12,24 @@ namespace Hospital.Application.ViewModels
     public class OPDBillViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public DateTime? Date { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int PatientId { get; set; }
         public string PatientName { get; set; }
         public PatientViewModel Patient { get; set; }
+
+        [Required]
         public string BillNo { get; set; }
         public string ReceiptNo { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int TotalAmount { get; set; }
+
         public int PayableAmount { get; set; }
         public string Description { get; set; }
         public virtual ICollection<OPDBillServiceViewModel> OPDBillServices { get; set; }
