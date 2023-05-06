77bab9a6c4e481135cfc1273a1fdcd4ac32bf2a5
Elias Sarband <elias.sarband@gmail.com>
2023-05-06 23:11:57 +0330

    Set Currency Setting

---
+++ Presentation/Hospital.Web.BlazorServer/Helpers/SettingHelper.cs
@@ -0,0 +1,49 @@
+﻿using Hospital.Application.Queries;
+using Hospital.Web.BlazorServer.Models;
+using Hospital.Web.BlazorServer.Models.Auth;
+using MediatR;
+using MudBlazor;
+using System.Data;
+
+namespace Hospital.Web.BlazorServer.Helpers
+{
+    public class SettingHelper
+    {
+        private readonly IMediator _mediator;
+
+        public SettingHelper(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<CurrencySetting> GetCurrencySetting()
+        {
+            CurrencySetting currencySetting = new CurrencySetting()
+            {
+                CurrencySymbol = "$",
+                CurrencyTextBoxFormat = "#,##0 $",
+                CurrencyGridCellFormat = "{0:#,##0 $}"
+            };
+
+            var hospitalInformation = await _mediator.Send(new GetHospitalInformationByIdQuery(1));
+
+            if (hospitalInformation != null)
+            {
+                if (String.IsNullOrEmpty(hospitalInformation.CurrencySymbol))
+                {
+                    currencySetting.CurrencySymbol = "$";
+                    currencySetting.CurrencyTextBoxFormat = "#,##0 $";
+                    currencySetting.CurrencyGridCellFormat = "{0:#,##0 $}";
+                }
+                else
+                {
+                    currencySetting.CurrencySymbol = hospitalInformation.CurrencySymbol;
+                    currencySetting.CurrencyTextBoxFormat = "#,##0 " + hospitalInformation.CurrencySymbol;
+                    currencySetting.CurrencyGridCellFormat = "{0:#,##0 " + hospitalInformation.CurrencySymbol + "}";
+                }
+            }
+
+            return currencySetting;
+        }
+    }
+}
